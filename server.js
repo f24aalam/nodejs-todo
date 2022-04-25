ca611dd253b98829c22bf83067c55462dc06f2c1
Faizan Aalam <f24aalam@gmail.com>
2022-04-25 17:28:21 +0530

    Added controller and routes

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to NodeJS Todo App"})
 })
 
+require('./app/routes/auth.routes')(app)
+require('./app/routes/user.routes')(app)
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
